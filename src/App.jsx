df916f0d1322f0d6f04d29ee5f1dd0f9931c5f15
unknown <bennett.shen@ehsn.com.tw>
2024-05-17 11:15:50 +0800

    update animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,44 @@ import docker from './assets/docker.png';
 
 export default function App() {
   return (
-    <main>
-      <h1>Scroll Driven Animation 💫</h1>
-      <section>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequatur, voluptates et eaque vel in illum id atque temporibus totam blanditiis molestias laborum ex corporis illo perspiciatis neque consequuntur beatae.
-          Alias amet doloribus quis nihil laborum iste sit cum cumque, placeat dolor accusamus tempora nesciunt. Eligendi excepturi assumenda quisquam debitis earum quo ducimus, labore fuga ullam quidem iusto! Molestiae, maiores.
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequatur, voluptates et eaque vel in illum id atque temporibus totam blanditiis molestias laborum ex corporis illo perspiciatis neque consequuntur beatae.
-          Alias amet doloribus quis nihil laborum iste sit cum cumque, placeat dolor accusamus tempora nesciunt. Eligendi excepturi assumenda quisquam debitis earum quo ducimus, labore fuga ullam quidem iusto! Molestiae, maiores.
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequatur, voluptates et eaque vel in illum id atque temporibus totam blanditiis molestias laborum ex corporis illo perspiciatis neque consequuntur beatae.
-          Alias amet doloribus quis nihil laborum iste sit cum cumque, placeat dolor accusamus tempora nesciunt. Eligendi excepturi assumenda quisquam debitis earum quo ducimus, labore fuga ullam quidem iusto! Molestiae, maiores.
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequatur, voluptates et eaque vel in illum id atque temporibus totam blanditiis molestias laborum ex corporis illo perspiciatis neque consequuntur beatae.
-          Alias amet doloribus quis nihil laborum iste sit cum cumque, placeat dolor accusamus tempora nesciunt. Eligendi excepturi assumenda quisquam debitis earum quo ducimus, labore fuga ullam quidem iusto! Molestiae, maiores.
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequatur, voluptates et eaque vel in illum id atque temporibus totam blanditiis molestias laborum ex corporis illo perspiciatis neque consequuntur beatae.
-          Alias amet doloribus quis nihil laborum iste sit cum cumque, placeat dolor accusamus tempora nesciunt. Eligendi excepturi assumenda quisquam debitis earum quo ducimus, labore fuga ullam quidem iusto! Molestiae, maiores.
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequatur, voluptates et eaque vel in illum id atque temporibus totam blanditiis molestias laborum ex corporis illo perspiciatis neque consequuntur beatae.
-          Alias amet doloribus quis nihil laborum iste sit cum cumque, placeat dolor accusamus tempora nesciunt. Eligendi excepturi assumenda quisquam debitis earum quo ducimus, labore fuga ullam quidem iusto! Molestiae, maiores.
-        </p>
-      </section>
+    <>
+      <div className="progress-bar"></div>
+      <main>
+        <h1>Scroll Driven Animation 💫</h1>
+        <section>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequatur, voluptates et eaque vel in illum id atque temporibus totam blanditiis molestias laborum ex corporis illo perspiciatis neque consequuntur beatae.
+            Alias amet doloribus quis nihil laborum iste sit cum cumque, placeat dolor accusamus tempora nesciunt. Eligendi excepturi assumenda quisquam debitis earum quo ducimus, labore fuga ullam quidem iusto! Molestiae, maiores.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequatur, voluptates et eaque vel in illum id atque temporibus totam blanditiis molestias laborum ex corporis illo perspiciatis neque consequuntur beatae.
+            Alias amet doloribus quis nihil laborum iste sit cum cumque, placeat dolor accusamus tempora nesciunt. Eligendi excepturi assumenda quisquam debitis earum quo ducimus, labore fuga ullam quidem iusto! Molestiae, maiores.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequatur, voluptates et eaque vel in illum id atque temporibus totam blanditiis molestias laborum ex corporis illo perspiciatis neque consequuntur beatae.
+            Alias amet doloribus quis nihil laborum iste sit cum cumque, placeat dolor accusamus tempora nesciunt. Eligendi excepturi assumenda quisquam debitis earum quo ducimus, labore fuga ullam quidem iusto! Molestiae, maiores.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequatur, voluptates et eaque vel in illum id atque temporibus totam blanditiis molestias laborum ex corporis illo perspiciatis neque consequuntur beatae.
+            Alias amet doloribus quis nihil laborum iste sit cum cumque, placeat dolor accusamus tempora nesciunt. Eligendi excepturi assumenda quisquam debitis earum quo ducimus, labore fuga ullam quidem iusto! Molestiae, maiores.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequatur, voluptates et eaque vel in illum id atque temporibus totam blanditiis molestias laborum ex corporis illo perspiciatis neque consequuntur beatae.
+            Alias amet doloribus quis nihil laborum iste sit cum cumque, placeat dolor accusamus tempora nesciunt. Eligendi excepturi assumenda quisquam debitis earum quo ducimus, labore fuga ullam quidem iusto! Molestiae, maiores.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam consequatur, voluptates et eaque vel in illum id atque temporibus totam blanditiis molestias laborum ex corporis illo perspiciatis neque consequuntur beatae.
+            Alias amet doloribus quis nihil laborum iste sit cum cumque, placeat dolor accusamus tempora nesciunt. Eligendi excepturi assumenda quisquam debitis earum quo ducimus, labore fuga ullam quidem iusto! Molestiae, maiores.
+          </p>
+        </section>
 
-      <div className="scene">
-        <div className="water"></div>
-        <img className="whale" src={docker}></img>
-        <h2 className="text">DOCKER</h2>
-      </div>
+        <div className="scene">
+          <div className="water"></div>
+          <img className="whale" src={docker}></img>
+          <h2 className="text">DOCKER</h2>
+        </div>
 
-    </main>
+      </main>
+    </>
   )
 }
